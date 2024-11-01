2a19ae598b029e1aa760ddb9500e8a8e58073009
AFRIDIBOT <afridibot8@gmail.com>
2024-11-01 20:57:00 +0600

    Update bal.js

--- scripts/cmds/bal.js
+++ scripts/cmds/bal.js
@@ -1,1 +1,22 @@
+module.exports = {
+ config: {
+	 name: "bal",
+	 version: "1.0",
+	 author: "AceGun",
+	 countDown: 5,
+	 role: 0,
+	 shortDescription: "no prefix",
+	 longDescription: "no prefix",
+	 category: "no prefix",
+ },
 
+ onStart: async function(){}, 
+ onChat: async function({ event, message, getLang }) {
+ if (event.body && event.body.toLowerCase() === "bal") {
+ return message.reply({
+ body: " 「 FUCK YOU 😰🖕\n\n𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\n~𝙲𝚈𝙱𝙴𝚁 𝙼𝙸𝙼~」",
+ attachment: await global.utils.getStreamFromURL("https://i.imgur.com/SBupaKE.jpeg")
+ });
+ }
+ }
+}
